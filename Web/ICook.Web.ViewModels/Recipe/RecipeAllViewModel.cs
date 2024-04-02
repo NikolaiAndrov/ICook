b389355f409616dc61e9401ccac3462d52591889
nikolaiandrov <nikolaiandrov@abv.bg>
2024-04-02 20:26:02 +0300

    RecipeAllViewModel and RecipePageViewModel added

---
+++ Web/ICook.Web.ViewModels/Recipe/RecipeAllViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ICook.Web.ViewModels.Recipe
+{
+	public class RecipeAllViewModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public int CategoryId { get; set; }
+
+		public string CategoryName { get; set; }
+	}
+}
+	
---
+++ Web/ICook.Web.ViewModels/Recipe/RecipePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ICook.Web.ViewModels.Recipe
+{
+	using System.Collections.Generic;
+
+	public class RecipePageViewModel
+	{
+		public int CurrentPage { get; set; }
+
+		public IEnumerable<RecipeAllViewModel> AllRecipes { get; set; }
+	}
+}
