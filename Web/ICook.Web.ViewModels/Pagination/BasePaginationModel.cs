3cbc8a08203160eedef92af1d5a2d290386f3683
nikolaiandrov <nikolaiandrov@abv.bg>
2024-04-03 13:56:52 +0300

    pagination implemented

---
+++ Web/ICook.Web.ViewModels/Pagination/BasePaginationModel.cs
@@ -0,0 +1,23 @@
+﻿namespace ICook.Web.ViewModels.Pagination
+{
+	using System;
+
+	public class BasePaginationModel
+	{
+		public int CurrentPage { get; set; }
+
+		public int RecipesCount { get; set; }
+
+		public int ItemsPerPage { get; set; }
+
+		public bool HasPreviousPage => this.CurrentPage > 1;
+
+		public bool HasNextPage => this.CurrentPage < this.PagesCount;
+
+		public int PreviousPage => this.CurrentPage - 1;
+
+		public int NextPage => this.CurrentPage + 1;
+
+		public int PagesCount => (int)Math.Ceiling((double)this.RecipesCount / this.ItemsPerPage);
+	}
+}
--- Web/ICook.Web.ViewModels/Recipe/RecipePageViewModel.cs
+++ Web/ICook.Web.ViewModels/Recipe/RecipePageViewModel.cs
@@ -1,26 +1,10 @@
 ﻿namespace ICook.Web.ViewModels.Recipe
 {
-	using System;
+	using ICook.Web.ViewModels.Pagination;
 	using System.Collections.Generic;
 
-	public class RecipePageViewModel
+	public class RecipePageViewModel : BasePaginationModel
 	{
-		public int CurrentPage { get; set; }
-
 		public IEnumerable<RecipeAllViewModel> AllRecipes { get; set; }
-
-		public int RecipesCount { get; set; }
-
-		public int ItemsPerPage { get; set; }
-
-		public bool HasPreviousPage => this.CurrentPage > 1;
-
-		public bool HasNextPage => this.CurrentPage < this.PagesCount;
-
-		public int PreviousPage => this.CurrentPage - 1;
-
-		public int NextPage => this.CurrentPage + 1;
-
-		public int PagesCount => (int)Math.Ceiling((double)this.RecipesCount / this.ItemsPerPage);
 	}
 }
